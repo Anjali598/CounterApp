7a13b90295125364cd581b62718167269055daff
Anjali598 <tanjali.devi3@gmail.com>
2025-02-06 22:33:19 +0530

    firstcommit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+//Stateless Functional component
+const NavBar = ({ totalCounters }) => {
+  console.log("NAV Bar -Render");
+  return (
+    <nav className="navbar bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/#">
+          Navbar{" "}
+          <span className="badge badge-pill bg-secondary">{totalCounters}</span>
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
